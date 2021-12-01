18153f0f596abd360d77a06e30de163823a6bafd
Sebastian Medina <seba.medinad@gmail.com>
2021-11-30 22:25:38 -0300

    lesson 43

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,25 @@
 import { Component } from 'react'
 
+const styles ={
+    producto:{
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0,0,0,0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px',
+    },
+    img:{
+        width: '100%',
+    }
+
+}
+
 class Producto extends Component {
     render() {
         const { producto } = this.props
         return (
-            <div>
-                <img alt={producto.name} src={producto.img} />
+            <div style={styles.producto}>
+                <img style={styles.img} alt={producto.name} src={producto.img} />
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
             </div>
