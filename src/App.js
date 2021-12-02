ece4f742e555dd83f3aea91353d7a49a2bbd5d91
Sebastian Medina <seba.medinad@gmail.com>
2021-12-01 23:30:59 -0300

    lesson 51 and 52

--- src/App.js
+++ src/App.js
@@ -10,16 +10,38 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
       { name: 'Arberja', price: 2500, img: '/productos/arbejas.jpg' },
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
-    ]
+    ],
+    carro: [],
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1,
+        }) : x)
+      return this.setState({ carro: newCarro });
+    }
+
+    this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.carro);
     return (
       <div >
-        <Navbar/>
+        <Navbar />
         <Layout>
-          <Title/>
+          <Title />
           <Productos
-            agregarAlCarro={() => console.log('No hace nada')}
+            agregarAlCarro={this.agregarAlCarro }
             productos={this.state.productos}
           />
         </Layout>
