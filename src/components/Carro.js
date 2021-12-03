052c687171b5f2ffd1828d9d0919af0e65dbe590
Sebastian Medina <seba.medinad@gmail.com>
2021-12-02 22:10:43 -0300

    lesson 54

--- src/components/Carro.js
+++ src/components/Carro.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import BubbleAlert from './BubbleAlert'
+import DetallesCarro from './DetallesCarro'
 
 const styles = {
     carro: {
@@ -29,6 +30,7 @@ class Carro extends Component {
                 <button style={styles.carro}>
                     Carro
                 </button>
+                <DetallesCarro carro={carro} />
             </div>
         )
     }
